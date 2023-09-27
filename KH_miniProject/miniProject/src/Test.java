f25d4af594cf1bfb27185b893057287a9f63ec73
mooni <43951879+moonjiye@users.noreply.github.com>
2023-09-27 17:35:34 +0900

    Add files via upload

---
+++ KH_miniProject/miniProject/src/Test.java
@@ -0,0 +1,54 @@
+import 메뉴.DAO.Master_DAO;
+import 메뉴.DAO.MenuDAO;
+//import 메뉴.DAO.ReviewDAO;
+import 메뉴.Mode.CustomerMode;
+import 메뉴.Mode.MasterMode;
+import 메뉴.Mode.OrderMode;
+import 메뉴.VO.MenuVO;
+import 메뉴.VO.OrderVO;
+import 메뉴.VO.ReviewVO;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+//        CustomerMode customerMode = new CustomerMode();
+//        customerMode.runCustomerMode();
+//        OrderMode orderMode = new OrderMode();
+//        orderMode.runOrderMode();
+//        List<OrderVO>list = orderMode.orderSelect();
+//        orderMode.orderSelectPrint(list);
+        //Scanner sc= new Scanner(System.in);
+        //String inputDateStr = sc.nextLine();
+
+//        customerMode.customerReturn();
+
+
+//        orderMode.viewOrderMode();
+//        List<OrderVO>viewList = orderMode.viewOrder();
+//        orderMode.viewOrederPrint(viewList);
+//
+//        orderMode.runOrderMode();
+
+////jsm123 so391!
+
+////        masterMode.runMasterMode();
+//        MenuDAO menuDAO = new MenuDAO();
+////        menuDAO.menuInsert();
+//        Master_DAO mDao = new Master_DAO();
+//        List<OrderVO> totalList = mDao.viewTotal();
+//        mDao.viewTotalPrint(totalList);
+
+//        List<MenuVO> list = menuDAO.menuSelect();
+//        menuDAO.menuSelectPrint(list);
+//
+//        MasterMode masterMode = new MasterMode();
+//        masterMode.runMasterMode();
+
+//        ReviewDAO reviewDAO = new ReviewDAO();
+//        List<ReviewVO> writelist = reviewDAO.writeReview();
+//        reviewDAO.viewReviewSelectPrint(writelist);
+    }
+}
